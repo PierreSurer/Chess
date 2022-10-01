4df4028e98e7dcdd2246ca127f1f2eb34da568ae
mathis <mathis.brossier@universite-paris-saclay.fr>
2022-10-01 15:25:50 +0200

    minor

--- pawnMoves.m
+++ pawnMoves.m
@@ -1,29 +1,29 @@
 function moves = pawnMoves(x, y, Board)
     moves = zeros(4, 2, 'int8');
-    idx = 1;
+    idx = 0;
     team = sign(Board(x, y));
     tmpY = y + team;
 
     % normal forward move
-    if(Board(x, tmpY) == 0) 
+    if(Board(x, tmpY) == 0)
+        idx = idx + 1;
         moves(idx, 1) = x;
         moves(idx, 2) = tmpY;
-        idx = idx + 1;
     end
 
     % double forward move (white)
     if(y == 2 && sign(Board(x, 2)) == 1) 
         if(Board(x, 3) == 0 && Board(x, 4) == 0)
+            idx = idx + 1;
             moves(idx, 1) = x;
             moves(idx, 2) = 4;
-            idx = idx + 1;
         end
         % double forward move (black)
     elseif(y == 7 && sign(Board(x, 7)) == -1) 
         if(Board(x, 6) == 0 && Board(x, 5) == 0)
+            idx = idx + 1;
             moves(idx, 1) = x;
             moves(idx, 2) = 5;
-            idx = idx + 1;
         end
     end
 
@@ -31,15 +31,15 @@ function moves = pawnMoves(x, y, Board)
     if(tmpX < 9)
         % diagonal attack move
         if(sign(Board(tmpX, tmpY)) == -team)
+            idx = idx + 1;
             moves(idx, 1) = tmpX;
             moves(idx, 2) = tmpY;
-            idx = idx + 1;
         else
             % en passant
             if(Board(tmpX, y) == -team * 9 && Board(tmpX, tmpY) == 0)
+                idx = idx + 1;
                 moves(idx, 1) = tmpX;
                 moves(idx, 2) = tmpY;
-                idx = idx + 1;
             end
         end
     end
@@ -47,18 +47,17 @@ function moves = pawnMoves(x, y, Board)
     if(tmpX > 0)
         % diagonal attack move
         if(sign(Board(tmpX, tmpY)) == -team)
+            idx = idx + 1;
             moves(idx, 1) = tmpX;
             moves(idx, 2) = tmpY;
-            idx = idx + 1;
         else
             % en passant
             if(Board(tmpX, y) == -team * 9 && Board(tmpX, tmpY) == 0)
+                idx = idx + 1;
                 moves(idx, 1) = tmpX;
                 moves(idx, 2) = tmpY;
-                idx = idx + 1;
             end
         end
     end    
-    moves = moves(1:(idx - 1),:);
-
+    moves = moves(1:idx, :);
 end
